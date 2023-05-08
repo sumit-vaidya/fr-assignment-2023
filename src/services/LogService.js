5657619ae84cb61307c5d036364800661cf3c454
sumit-vaidya <vaidyasumit007@gmail.com>
2023-05-08 13:40:45 +0200

    updated api

--- src/services/LogService.js
+++ src/services/LogService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const LOG_API_BASE_URL = "http://localhost:8080/log/v1/logs";
+const LOG_API_BASE_URL = "http://localhost:8080/log/v1";
 
 class LogService {
   getLogs(logType) {
-    return axios.get(LOG_API_BASE_URL.concat("/").concat(logType));
+    return axios.get(LOG_API_BASE_URL.concat("/").concat(logType).concat("/logs"));
   }
 }
 
